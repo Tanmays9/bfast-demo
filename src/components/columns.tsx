90785425d08ae50e736cb2f8ec888df5e01be64f
Tanmay Sharma <tanmays040@gmail.com>
2025-05-03 11:14:39 +0530

    table fix

--- src/components/columns.tsx
+++ src/components/columns.tsx
@@ -2,7 +2,7 @@ import { Checkbox } from "@radix-ui/react-checkbox"
 import { ColumnDef } from "@tanstack/react-table"
 // import { Payment } from "./data-table"
 import { Item } from "@/lib/types"
-import {  Download } from "lucide-react"
+import { Download } from "lucide-react"
 import { Button } from "./ui/button"
 import { generateSingleCSV } from "@/lib/zip"
 
@@ -33,160 +33,78 @@ export const columns: ColumnDef<Item>[] = [
   },
   {
     accessorKey: "email",
-    header:() => <div className="text-right">Download PDF</div>,
+    header: () => <div className="text-right">Download PDF</div>,
     cell: ({ row }) => {
       return (
         <Button
           variant="ghost"
           onClick={() => generateSingleCSV(row.index)}
         >
-          
-          <Download/>
+
+          <Download />
         </Button>
       )
     },
   },
   {
     accessorKey: "awb",
-    header: "AWB",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("awb")}</div>
+    accessorFn: (row) => (
+      <>
+        <div className="capitalize">AWB: {row.awb}</div>
+        <div className="capitalize">Client Order ID: {row.cid}</div>
+        <div className="capitalize">Order Creation Date: {row.Order_Creation_Date}</div>
+      </>
     ),
-  },
-  {
-    accessorKey: "cid",
-    header: "Client Order ID",
+    header: "Order Details",
     cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("cid")}</div>
-    ),
-  },
-  {
-    accessorKey: "Order_Creation_Date",
-    header: "Order Creation Date",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("Order_Creation_Date")}</div>
+      row.getValue("awb")
     ),
   },
   {
     accessorKey: "name",
-    header: "Name",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("name")}</div>
+    accessorFn: (row) => (
+      <>
+        <div className="capitalize">Name: {row.name}</div>
+        <div className="capitalize">Phone: {row.phone}</div>
+        <div className="capitalize text-wrap">Address: {row.address}</div>
+        <div className="capitalize">Pincode: {row.pincode}</div>
+      </>
     ),
-  },
-  {
-    accessorKey: "phone",
-    header: "Phone",
+    header: "Consignee Details",
     cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("phone")}</div>
+      row.getValue("name")
     ),
-  },
-  {
-    accessorKey: "address",
-    header: "Address",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("address")}</div>
-    ),
-  },
-  {
-    accessorKey: "pincode",
-    header: "Pincode",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("pincode")}</div>
-    ),
+    size:200
+    ,
   },
   {
     accessorKey: "quantity",
-    header: "Quantity",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("quantity")}</div>
+    accessorFn: (row) => (
+      <>
+        <div className="capitalize">Quantity: {row.quantity}</div>
+        <div className="capitalize">Payment Mode: {row.Payment_Mode}</div>
+        <div className="capitalize">COD Amount: {row.codamount}</div>
+      </>
     ),
-  },
-  {
-    accessorKey: "Payment_Mode",
-    header: "Payment Mode",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("Payment_Mode")}</div>
-    ),
-  },
-  {
-    accessorKey: "codamount",
-    header: "COD Amount",
+    header: "Product Details",
     cell: ({ row }) => (
       <div className="capitalize">{row.getValue("quantity")}</div>
     ),
   },
   {
     accessorKey: "Delivery_Center_Name",
-    header: "Delivery Center Name",
+    accessorFn: (row) => (
+      <>
+        <div className="capitalize">Delivery Center Name: {row.Delivery_Center_Name}</div>
+        <div className="capitalize">Courier Partner: {row.Courier_Partner}</div>
+        <div className="capitalize">Transport Mode: {row.Transport_Mode}</div>
+        <div className="capitalize">Zone: {row.zone}</div>
+      </>
+    ),
+    header: "Shipping Details",
     cell: ({ row }) => (
       <div className="capitalize">{row.getValue("Delivery_Center_Name")}</div>
     ),
   },
-  {
-    accessorKey: "Courier_Partner",
-    header: "Courier Partner",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("Courier_Partner")}</div>
-    ),
-  },
-  {
-    accessorKey: "Transport_Mode",
-    header: "Transport Mode",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("Transport_Mode")}</div>
-    ),
-  },
-  {
-    accessorKey: "zone",
-    header: "Zone",
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("zone")}</div>
-    ),
-  },
-  
-  {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"))
 
-      // Format the amount as a dollar amount
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount)
-
-      return <div className="text-right font-medium">{formatted}</div>
-    },
-  },
-  // {
-  //   id: "actions",
-  //   enableHiding: false,
-  //   cell: () => {
-  //     // const payment = row.original
-
-  //     return (
-  //       <DropdownMenu>
-  //         <DropdownMenuTrigger asChild>
-  //           <Button variant="ghost" className="h-8 w-8 p-0">
-  //             <span className="sr-only">Open menu</span>
-  //             <MoreHorizontal />
-  //           </Button>
-  //         </DropdownMenuTrigger>
-  //         <DropdownMenuContent align="end">
-  //           <DropdownMenuLabel>Actions</DropdownMenuLabel>
-  //           {/* <DropdownMenuItem
-  //             onClick={() => navigator.clipboard.writeText(payment.id)}
-  //           >
-  //             Copy payment ID
-  //           </DropdownMenuItem> */}
-  //           {/* <DropdownMenuSeparator /> */}
-  //           <DropdownMenuItem>Download Shipping Label</DropdownMenuItem>
-  //           {/* <DropdownMenuItem>View payment details</DropdownMenuItem> */}
-  //         </DropdownMenuContent>
-  //       </DropdownMenu>
-  //     )
-  //   },
-  // },
 ]
